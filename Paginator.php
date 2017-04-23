9a8fb72b5e1c35edc04da5ee7b5a0a88eab82963
Junius LEKGWARA <Junius.Lekgwara@standardbank.co.za>
2017-04-24 01:33:37 +0200

    removed old files and modified Paginator

--- Paginator.php
+++ Paginator.php
@@ -8,7 +8,43 @@
 class Paginator
 {
     private $_db;
+    private $_table = null;
     private $_hide = 'style="display: none !important"';
+    private $_currentItemClass = '';
+    private $_itemLimitPerPage;
+    private $_rowOffset = 0;
+
+    /**
+     * @return mixed
+     */
+    public function getItemLimitPerPage()
+    {
+        return $this->_itemLimitPerPage;
+    }
+
+    /**
+     * @param mixed $limitItems
+     */
+    public function setItemLimitPerPage($limitItems)
+    {
+        $this->_itemLimitPerPage = $limitItems;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRowOffset()
+    {
+        return $this->_rowOffset;
+    }
+
+    /**
+     * @param mixed $rowOffset
+     */
+    public function setRowOffset($rowOffset)
+    {
+        $this->_rowOffset = $rowOffset;
+    }
 
 
     /**
@@ -29,6 +65,22 @@ class Paginator
     /**
      * @return mixed
      */
+    public function getTable()
+    {
+        return $this->_table;
+    }
+
+    /**
+     * @param mixed $table
+     */
+    public function setTable($table)
+    {
+        $this->_table = $table;
+    }
+
+    /**
+     * @return mixed
+     */
     public function getHide()
     {
         return $this->_hide;
@@ -40,6 +92,187 @@ class Paginator
     public function setHide($hide)
     {
         $this->_hide = $hide;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrentItemClass()
+    {
+        return $this->_currentItemClass;
+    }
+
+    /**
+     * @param string $currentItemClass
+     */
+    public function setCurrentItemClass($currentItemClass)
+    {
+        $this->_currentItemClass = $currentItemClass;
+    }
+
+    // get row count
+    public function rowCount($table = null)
+    {
+        if ($this->_table === null && $table === null) {
+            throw new Exception("Table was not set");
+        } else {
+            if ($table !== null) {
+                $stmt = $this->_db->prepare("SELECT * FROM $table");
+                $stmt->execute();
+                return $stmt->rowCount();
+            } elseif ($this->_table !== null) {
+                $stmt = $this->_db->prepare("SELECT * FROM $this->_table");
+                $stmt->execute();
+                return $stmt->rowCount();
+            }
+        }
+    }
+
+    public function getRowsLeft($table = null)
+    {
+        if ($this->getCurrentPage() !== 'index.php') {
+            $this->_rowOffset = ($this->_itemLimitPerPage * $this->getPageNumber());
+        }
+        if ($this->_table === null && $table === null) {
+            throw new Exception("Table was not set");
+        } else {
+            if ($table !== null) {
+                $stmt = $this->_db->prepare("SELECT * FROM $table LIMIT $this->getRowOffset(), $this->getLimitItems()");
+                $stmt->execute();
+                return $stmt->rowCount();
+            } elseif ($this->_table !== null) {
+                $stmt = $this->_db->prepare("SELECT * FROM $this->_table LIMIT $this->getRowOffset(), $this->getLimitItems()");
+                $stmt->execute();
+                return $stmt->rowCount();
+            }
+        }
+    }
+
+    public function getPageData($colId, $params = [])
+    {
+        if ($this->_table === null && !isset($params['table'])) {
+            throw new Exception("Table was not set");
+        }
+        $columns = ($params['columns']) ? $params['columns'] : '*';
+        $sort = ($params['sort']) ? $params['sort'] : 'DESC';
+        if ($params['table'] !== null) {
+            $table = $params['table'];
+            $rowsLeft = $this->getRowsLeft($table);
+            if ($rowsLeft < $this->_itemLimitPerPage) {
+                $this->_itemLimitPerPage = $rowsLeft;
+            }
+            $select = "SELECT $columns FROM " . $table['table'] . "ORDER BY $colId $sort LIMIT ?,?";
+            $prepare = $this->_db->prepare($select);
+            $prepare->bindParam(1, $this->getRowOffset(), PDO::PARAM_INT);
+            $prepare->bindParam(2, $this->getItemLimitPerPage(), PDO::PARAM_INT);
+            $prepare->execute();
+            $results = $prepare->fetchAll();
+            return $results;
+        } elseif ($this->_table !== null) {
+            $rowsLeft = $this->getRowsLeft($this->_table);
+            if ($rowsLeft < $this->_itemLimitPerPage) {
+                $this->_itemLimitPerPage = $rowsLeft;
+            }
+            $prepare = $this->_db->prepare("SELECT * FROM $this->_table ORDER BY $colId $sort LIMIT $this->getRowOffset(), $this->getLimitItems()");
+            $prepare->execute();
+            $results = $prepare->fetchAll();
+            return $results;
+        }
+    }
+
+    function prevPages($pageNumber, $numPrevPages)
+    {
+        $listItems = ''; // to save all list items.
+        while ($numPrevPages >= 1) {
+            $pageNumber -= 1;
+            if ($pageNumber >= 1) {
+                $page = $pageNumber . '.php';
+                if (file_exists("$page")) {
+                    $listItems = '<li><a href="' . $pageNumber . '.php">' . $pageNumber . '</a></li>' . $listItems;
+                }
+            }
+            $numPrevPages -= 1;
+        }
+        return $listItems;
+    }
+
+    function nextPages($pageNumber, $numNextPages)
+    {
+        $listItems = ''; // to save list items.
+        $count = 1;
+        while ($count <= $numNextPages) {
+            $pageNumber += 1;
+            $page = $pageNumber . '.php';
+            if (file_exists("$page")) {
+                $listItems .= '<li><a href="' . $pageNumber . '.php">' . $pageNumber . '</a></li>';
+            }
+            $count += 1;
+        }
+        return $listItems;
+    }
+
+    function pagination($pageNumber, $numPrevPages, $numNextPages, $paginationCssClasses = '')
+    {
+
+        $prevPagesList = '<ul class="' . $paginationCssClasses . '"">' . $this->prevButton($pageNumber) . $this->prevPages($pageNumber, $numPrevPages);
+        $nextPageList = $this->nextPages($pageNumber, $numNextPages) . $this->nextButton($pageNumber) . '</ul>';
+        if ($pageNumber == 'index') {
+            $listItems = $prevPagesList . $nextPageList;
+        }else {
+            $listItems = $prevPagesList . '<li class="' . $this->getCurrentItemClass() . '"><a href="">' . $pageNumber . '</a> </li>' . $nextPageList;
+        }
+        echo $listItems;
+    }
+
+    function prevButton($pageNumber)
+    {
+        $prev = '';
+       if ($pageNumber == 1) {
+            $prev = '<li><a href="index.php">&laquo; Previous</a></li>';
+        } elseif ($pageNumber > 1) {
+            $prev = '<li><a href="' . ($pageNumber - 1) . '.php' . '">&laquo; Previous</a></li>';
+        }
+        return $prev;
+    }
+
+    function nextButton($pageNumber)
+    {
+        if ($pageNumber == 'index') {
+            $page = '1.php';
+        } else {
+            $page = ($pageNumber + 1) . '.php';
+        }
+        if (file_exists($page)) {
+            return '<li><a href="' . ($pageNumber + 1) . '.php">Next &raquo; </a></li>';
+        }
+        return '';
+    }
+
+    function getPageNumber()
+    {
+        $currentPage = basename($_SERVER['SCRIPT_FILENAME']);
+        $pageNumber = rtrim($currentPage, '.php');
+        return $pageNumber;
+    }
+
+    function getCurrentPage()
+    {
+        $currentPage = basename($_SERVER['SCRIPT_FILENAME']);
+        return $currentPage;
+    }
+
+    function createPages()
+    {
+        $last_page = ($this->rowCount() / $this->getItemLimitPerPage()) - 1;
+        if (!is_int($last_page)) {
+            $last_page = (int)$last_page + 1;
+        }
+        for ($counter = 1; $counter <= $last_page; $counter++) {
+            $page = $counter . '.php';
+            if (!file_exists($page)) {
+                copy('index.php', $page);
+            }
+        }
     }
 
     public function setColumnCaseAttribute($case)
@@ -59,7 +292,8 @@ class Paginator
         }
     }
 
-    public function setErrorModeAttribute($error) {
+    public function setErrorModeAttribute($error)
+    {
         switch ($error) {
             case 'silent':
                 $this->_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
@@ -75,4 +309,49 @@ class Paginator
         }
     }
 
+    public function setOracleNullsAttribute($error)
+    {
+        switch ($error) {
+            case 'null natural':
+                $this->_db->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_NATURAL);
+                break;
+            case 'null empty string':
+                $this->_db->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+                break;
+            case 'null to string':
+                $this->_db->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_TO_STRING);
+                break;
+            default:
+                throw new Exception("Unknown Oracle null attribute.");
+        }
+    }
+
+    public function setStringifyFetchesAttribute($bool)
+    {
+        if (is_bool($bool)) {
+            $this->_db->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, $bool);
+        } else {
+            throw new Exception("Function requires boolean value, unknown type provided.");
+        }
+    }
+
+    public function setStatementClassAttribute($custom)
+    {
+        $this->_db->setAttribute(PDO::ATTR_STATEMENT_CLASS, array($custom, array($this->_db)));
+    }
+
+    public function setTimeoutAttribute($seconds)
+    {
+        $this->_db->setAttribute(PDO::ATTR_TIMEOUT, $seconds);
+    }
+
+    public function setAutoCommitAttribute($bool)
+    {
+        $this->_db->setAttribute(PDO::ATTR_AUTOCOMMIT, $bool);
+    }
+
+    public function setEmulatePreparesAttribute($bool)
+    {
+        $this->_db->setAttribute(PDO::ATTR_EMULATE_PREPARES, $bool);
+    }
 }
--- index.php
+++ index.php
@@ -1,91 +1,46 @@
 <?php
 
-require_once('pagination.php');
-require_once('Db.php');
+require_once 'Paginator.php';
 
-$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-$Db = new Db('mysql:host=localhost;dbname=data','root','',$options);
+$Paginator = new Paginator('mysql:host=localhost;dbname=ng_app', 'root', '000000');
+$Paginator->setItemLimitPerPage(12);
+$Paginator->setTable('comments');
+$Paginator->createPages();
 
-// get row count
-$row_count = $Db->rowCounts('testing'); // "testing" is the name of the table
-
-// how many records should be displayed on a page?
-$num_rows = 12;
-
-// get last page 
-$last_page = ($row_count / $num_rows) - 1;
-if(!is_int($last_page)){
-    $last_page = (int)$last_page + 1;
-}
-$start = 0; // starting position (offset)
-$limit = $num_rows; // number of records to return 
-
-if($current_page !== 'index.php'){
-    $start = ($limit * $page_number);
-}
-// get rows left in the database
-$rows_left = $Db->rowsLeft('testing', $start, $limit);
-
-if($rows_left < $num_rows){
-    $limit = $num_rows;
-}
-
-// selecting data from the database
-$select = $Db->selecetLimit('testing', '*', $start, $limit);
-
-// from the data we have, we can now create pages by looping through the $last_page.
-// we are going to create pages until the we reach the last page, by copying index.php or some other page.
-for ($counter = 1; $counter <= $last_page; $counter++) { 
-    $page = $counter . '.php';
-    if(!file_exists($page)){
-        copy('index.php', $page);
-    }
-}
 ?>
 <!DOCTYPE html>
-<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
-<html class="no-js" lang="en" >
+<!--[if IE 9]>
+<html class="lt-ie10" lang="en"> <![endif]-->
+<html class="no-js" lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <!-- If you delete this meta tag World War Z will become a reality -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Foundation 5</title>
-
-  <!-- If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/foundation.css">
-
-  <!-- If you are using the gem version, you need this only -->
-  <link rel="stylesheet" href="css/app.css">
-
-  <script src="js/vendor/modernizr.js"></script>
+    <meta charset="utf-8">
+    <!-- If you delete this meta tag World War Z will become a reality -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foundation 5</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/css/foundation.min.css" integrity="sha256-itWEYdFWzZPBG78bJOOiQIn06QCgN/F0wMDcC4nOhxY=" crossorigin="anonymous" />
 
 </head>
 <body>
 
 <div class="row">
-     <div class="column small-12 medium-9 large-9">
-  <!-- body content here -->
-  <?php 
-  	//loop through the data from the database
-   ?>
-   <div class="pagination-centered">
-   		<!-- our paginatio  using foundation 5-->
-   		 <hr>
-             <ul class="pagination" role="menubar" arial-label="Pagination">
-                 <li class="arrow" arial-disabled="true"><a href="<?php echo ($page_number - 1).'.php'; ?>">&laquo; Previous</a></li>
-                 <!-- pagination goes here -->
-                 <?php pagination($page_number, 4, 4); ?>
-                 <li class="arrow" arial-disabled="true"><a href="<?php echo ($page_number + 1).'.php'; ?>">Next &raquo; </a></li>
-             </ul>
-   </div>
-	</div>
+    <div class="column small-12 medium-9 large-9">
+        <!-- body content here -->
+        <?php
+        //loop through the data from the database
+        ?>
+        <div class="pagination-centered">
+            <!-- our paginatio  using foundation 5-->
+            <hr>
+            <?php $Paginator->pagination($Paginator->getPageNumber(), 4, 4, 'pagination'); ?>
+
+        </div>
+    </div>
 </div>
-  <script src="js/vendor/jquery.js"></script>
-  <script src="js/foundation.min.js"></script>
-  <script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.3.1/js/foundation.min.js" integrity="sha256-Nd2xznOkrE9HkrAMi4xWy/hXkQraXioBg9iYsBrcFrs=" crossorigin="anonymous"></script>
+<script>
     $(document).foundation();
-  </script>
+</script>
 </body>
 </html>
